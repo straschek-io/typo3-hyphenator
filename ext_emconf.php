2633687bdee364d740644acc5e329765c8e5e700
Michael Straschek <m@straschek.io>
2022-02-19 09:42:22 +0000

    Mark extension as compatible with TYPO3 11 (no further changes needed)

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF['typo3_hyphenator'] = [
     'author_email' => 'hallo@straschek.io',
     'state' => 'stable',
     'clearCacheOnLoad' => false,
-    'version' => '1.2.0',
+    'version' => '1.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.99.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
     ],
 ];
